6fa209bf0e5af3be339e7d27396c11ae62cdbfe7
Narendra Kumar <96626813+narendrakr9870@users.noreply.github.com>
2025-09-07 00:43:47 +0530

    Update apis.js for course categories

--- src/services/apis.js
+++ src/services/apis.js
@@ -28,7 +28,7 @@ export const courseEndpoints = {
   GET_ALL_COURSE_API: `${BASE_URL}/api/v1/course/getAllCourses`,
   COURSE_DETAILS_API: `${BASE_URL}/api/v1/course/getCourseDetails`,
   EDIT_COURSE_API: `${BASE_URL}/api/v1/course/editCourse`,
-  // COURSE_CATEGORIES_API: `${BASE_URL}/api/v1/course/showAllCategories`,
+  COURSE_CATEGORIES_API: `${BASE_URL}/api/v1/course/showAllCategories`,
   CREATE_COURSE_API: `${BASE_URL}/api/v1/course/createCourse`,
   CREATE_SECTION_API: `${BASE_URL}/api/v1/course/createSection`,
   CREATE_SUBSECTION_API: `${BASE_URL}/api/v1/course/createSubSection`,
